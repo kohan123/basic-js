b68480283a348f705d977145a06dd4fcd07f08c0
Danil Kohan <Sportodavka@gmail.com>
2020-10-31 20:06:45 +0300

    feat: solve carbon, cats, dream tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
+const APROXIMATION = 0.9914;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+ const sampleNumber = Number(sampleActivity);
+ if (String(sampleNumber) !== sampleActivity) {
+   return false;
+ } else if (sampleNumber <= 0 || sampleNumber > MODERN_ACTIVITY) {
+   return false;
+ } else {
+   const K = LN2 / HALF_LIFE_PERIOD;
+   return Math.round((Math.log(MODERN_ACTIVITY/sampleNumber) / APROXIMATION) / K);
+ }
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+ if (!Array.isArray(members)) {
+   return false; 
+   } else {
+     return members.map(curr => {
+     if (typeof curr === "string") {
+       return curr.trim()[0].toUpperCase();
+     } else {
+       return null;
+     }
+     }).filter(el => el !== null).sort().join("");
+   }
+ };
+
